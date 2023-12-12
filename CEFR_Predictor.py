c56647cbdfd2dd3412101c269a47c88c4eacfb7f
Melvin Petracca <38191338+melvin2504@users.noreply.github.com>
2023-12-12 20:58:30 +0100

    Add files via upload

---
+++ CEFR_Predictor.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from model_predictor import FrenchSentenceDifficultyPredictor
+
+model_path = r'E:/hec masters/DSML/cefr_predictor/model/modelforApp.pth'
+label_encoder_path = r'E:/hec masters/DSML/cefr_predictor/model/label_encoderforApp.pkl'
+
+# Create an instance of the predictor
+predictor = FrenchSentenceDifficultyPredictor(model_path, label_encoder_path)
+
+st.title("French Sentence Difficulty Predictor")
+
+# User input
+user_input = st.text_area("Enter a French sentence:", height=150)
+
+# Button for predictions
+if st.button("Predict Difficulty"):
+    # Get predictions
+    difficulty, scores = predictor.predict_difficulty(user_input)  # Modify your method to return both label and scores
+
+    # Display the predicted difficulty level
+    st.write(f"The predicted difficulty level is: {difficulty}")
+
+    # Display scores for all categories if available
+    if scores:
+        st.write("Scores for each difficulty level:")
+        for label, score in scores.items():  # Assuming scores is a dict with {label: score}
+            st.write(f"{label}: {score}")
